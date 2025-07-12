ffa1ea66326383070bf5989d0b3a790f0ab2ec36
ronik297 <ronik.allen@gmail.com>
2025-07-12 16:43:38 +0530

    added file upload functionality

--- src/app/(dashboard)/_components/upload-button.tsx
+++ src/app/(dashboard)/_components/upload-button.tsx
@@ -2,14 +2,64 @@ import { Button } from '@/components/ui/button'
 import { RiFileAddFill } from '@remixicon/react'
 import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
-import { useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'sonner'
+import axios from 'axios'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { P } from '@/components/custom/p'
 
 const UploadButton = () => {
     const pathname = usePathname();
     const queryClient = useQueryClient();
     const [fileProgress, setFileProgress] = useState<Record<string, number>>()
     const [isUploading, setIsUploading] = useState(false)
+
+    async function uploadFile(file: File) {
+        const formData = new FormData();
+
+        const res = await axios.post('/api/v1/files/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+            onUploadProgress: (progressEvent) => {
+                const total = progressEvent.total || 1;
+                const loaded = progressEvent.loaded || 0;
+                const percent = Math.round((loaded/total) * 100);
+                setFileProgress((prev) => ({
+                    ...prev,
+                    [file.name]: percent
+                }));
+            }
+        })
+
+        return res.data;
+    }
+
+    const mutation = useMutation({
+        mutationFn: uploadFile,
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ 
+                queryKey: ['files', data.category],
+            })
+
+            toast(data?.message, {
+                description: data?.description,
+            })
+        },
+        onError: (error) => {
+            toast.error(error.name, {
+                description: error instanceof Error ? error.message : "An error occurred while uploading the file",
+            })
+        },
+        onSettled: () => {
+
+        }
+    })
 
     async function handleFileChange(e: React.ChangeEvent<HTMLInputElement>) {
         const files = e.target.files ? Array.from(e.target.files) : [];
@@ -30,20 +80,74 @@ const UploadButton = () => {
 
         setFileProgress(progressMap);
         console.log("progressMap:", progressMap);
+
+        setIsUploading(true);
+
+         await Promise.all(files.map((file) => mutation.mutateAsync(file)));
+        
+        e.target.value = '';
     }
 
-  return (
-    <>
-        <Button onClick={() => {
-            document.getElementById('file-upload')?.click()
-        }}>
+    return (
+        <>
+        {isUploading &&
+            Object.entries(fileProgress ?? {}).map(([fileName, progress], i) => (
+            <TooltipProvider key={i}>
+                <Tooltip>
+                <TooltipTrigger>
+                    <div className="relative size-9 rounded-full flex items-center justify-center drop-shadow-md cursor-default animate-pulse">
+                        <svg
+                            className="absolute w-full h-full transform -rotate-90"
+                            viewBox="0 0 36 36"
+                        >
+                                <circle                            className="text-gray-300"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                                fill="transparent"
+                                r="16"
+                                cx="18"
+                                cy="18"
+                            />
+                            <circle
+                                className="text-primary"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                                strokeLinecap="round"
+                                fill="transparent"
+                                r="16"
+                                cx="18"
+                                cy="18"
+                                strokeDasharray="100"
+                                strokeDashoffset={100 - progress}
+                            />
+                        </svg>
+                        <P className="text-xs text-primary font-bold">      {progress}%
+                        </P>
+                    </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                    <P className="text-xs font-bold">{fileName}</P>
+                </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+            ))}
+        <Button
+            onClick={() => {
+            document.getElementById("file-upload")?.click();
+            }}
+        >
             <RiFileAddFill /> Upload
         </Button>
-        <input type='file' className='hidden' id="file-upload" 
-        multiple
-        onChange={handleFileChange} />
+
+        <input
+            type="file"
+            className="hidden"
+            id="file-upload"
+            multiple
+            onChange={handleFileChange}
+        />
     </>
-  )
+  );
 }
 
 export default UploadButton
